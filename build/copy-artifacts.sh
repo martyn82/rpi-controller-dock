70e750f64acc9f1923ebe7ef964fea812a2c8bf4
Martijn Endenburg <martijn.endenburg@gmail.com>
2015-06-15 15:02:09 +0200

    Use separate containers for build and dist of app

---
+++ build/copy-artifacts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ~/rpi-controller/bin/controller-server /dist/controller-server \
+    && cp ~/rpi-controller/bin/controller /dist/controller
--- start-rpi-controller.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 1181:1181 martyn82/rpi-controller
